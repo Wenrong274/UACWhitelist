36e8b9a8d913097c38d399f138ad367354cc4475
Wen Rong <hybrid0930@gmail.com>
2019-11-04 17:11:51 +0800

    [add] uac tool

--- UACWhitelist/Form1.cs
+++ UACWhitelist/Form1.cs
@@ -12,9 +12,42 @@ namespace UACWhitelist
 {
     public partial class Form1 : Form
     {
+        RegEditWhiteList regEditWhiteList = null;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Multiselect = false;
+            dialog.Filter = "EXE(*.EXE)|*.EXE|" + "所有檔案|*.*";
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = dialog.FileName;
+                regEditWhiteList = new RegEditWhiteList(dialog.FileName);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (regEditWhiteList == null)
+            {
+                MessageBox.Show("執行檔路徑錯誤", "錯誤", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (System.IO.File.Exists(regEditWhiteList.keyName))
+            {
+                regEditWhiteList.SendRegedit();
+                MessageBox.Show("新增成功", "成功", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("執行檔路徑錯誤", "錯誤", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
         }
     }
 }
---
+++ UACWhitelist/RegEditWhiteList.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+using Microsoft.Win32;
+
+public class RegEditWhiteList
+{
+    public string keyName { get; set; }
+    private readonly string root = @"Software\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Layers";
+    private readonly string keyValue = "~ RunAsInvoker";
+
+    public RegEditWhiteList(string keyName)
+    {
+        this.keyName = keyName;
+    }
+
+    public void SendRegedit()
+    {
+        RegistryKey key = Registry.CurrentUser.OpenSubKey(root, true);
+        key.SetValue(keyName, keyValue,RegistryValueKind.String);
+        key.Close();
+    }
+}
